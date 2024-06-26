ca95a31443836a661f12d68219e0825e145be1b6
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-06-26 18:34:39 +0300

    Initial commit

---
+++ src/main/java/com/example/todolistapi/ToDoListApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todolistapi/ToDoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todolistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
