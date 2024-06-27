81b80de5614de02b4ed7d9470d4c2f04a0ee98cc
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-06-27 19:24:12 +0300

    modifying build and main class

--- src/main/java/com/example/todolistapi/ToDoListApiApplication.java
+++ src/main/java/com/example/todolistapi/ToDoListApiApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ToDoListApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ToDoListApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListApiApplication.class, args);
+        System.out.println("DB Username: " + System.getenv("DB_USERNAME"));
+        System.out.println("DB Password: " + System.getenv("DB_PASSWORD"));
+    }
 }
