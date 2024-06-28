0c5bef840f1b48ec5d33a417d0b1e8c96f035ded
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-06-28 16:26:55 +0300

    nothing new

--- src/main/java/com/example/todolistapi/ToDoListApiApplication.java
+++ src/main/java/com/example/todolistapi/ToDoListApiApplication.java
@@ -8,7 +8,5 @@ public class ToDoListApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ToDoListApiApplication.class, args);
-        System.out.println("DB Username: " + System.getenv("DB_USERNAME"));
-        System.out.println("DB Password: " + System.getenv("DB_PASSWORD"));
     }
 }
