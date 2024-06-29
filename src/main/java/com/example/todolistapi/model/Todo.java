fb1f8f851512d97af0e25b95e0877522d76dc053
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-06-29 18:14:32 +0300

    using lombok instead of basic getters/setters in Todo.java

--- src/main/java/com/example/todolistapi/model/Todo.java
+++ src/main/java/com/example/todolistapi/model/Todo.java
@@ -5,14 +5,31 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Data
 public class Todo {
+    @Setter
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Setter
+    @Getter
     private String title;
+    @Setter
+    @Getter
     private String description;
     private boolean completed;
+
+    public Todo() {
+
+    }
+
+    public Todo(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
 }
