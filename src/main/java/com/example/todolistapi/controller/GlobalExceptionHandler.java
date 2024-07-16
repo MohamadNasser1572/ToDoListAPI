737de2a3e9167647e1589b7a46cccb6fc69a652f
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-07-16 11:41:26 +0300

    improve exception handling

---
+++ src/main/java/com/example/todolistapi/controller/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.example.todolistapi.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.ResponseEntity;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity.status(500).body(e.getMessage());
+    }
+}
--- src/main/java/com/example/todolistapi/model/Todo.java
+++ src/main/java/com/example/todolistapi/model/Todo.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Todo {
@@ -11,10 +12,13 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Title is mandatory")
     private String title;
+
     private String description;
 
-    // Getters and Setters
+
     public Long getId() {
         return id;
     }
