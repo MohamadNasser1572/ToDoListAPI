29d4a93e600422fb085d03b71bed33f727fa6a71
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-07-23 10:10:53 +0300

    modifying build.gradle and security config

--- src/main/java/com/example/todolistapi/config/SecurityConfig.java
+++ src/main/java/com/example/todolistapi/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.todolistapi.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -8,12 +9,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
+    public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withDefaultPasswordEncoder()
                 .username("user")
@@ -26,11 +29,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize
+                .authorizeHttpRequests((authorize) -> authorize
                         .anyRequest().authenticated()
                 )
                 .formLogin(withDefaults());
         return http.build();
     }
 }
-
